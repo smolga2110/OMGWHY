78feb2a177e1808ba575f6abd39a9ca4aee63e0c
smolga2110 <73380083+smolga2110@users.noreply.github.com>
2020-11-09 10:29:14 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from ui_functions import *
+#from ui_functions import *
 from PySide2.QtWidgets import *
 import os
 import sys
 import platform
 import sqlite3
+import datetime
 import youtube_dl
 from PySide2.QtSql import *
 from PySide2 import QtCore, QtGui, QtWidgets
@@ -13,7 +14,12 @@ from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFo
 from ui_main import Ui_MainWindow
 
 from ui_styles import Style
+from runing import Run
 
+count = 1
+
+GLOBAL_STATE = 0
+GLOBAL_TITLE_BAR = True
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -23,6 +29,7 @@ class MainWindow(QMainWindow):
         self.mythread = Run()
         self.url = None
         self.download_folder = None
+
         self.con = sqlite3.connect("history_db.sqlite")
         self.cur = self.con.cursor()
         self.mythread.mysignal.connect(self.handler)
@@ -37,6 +44,8 @@ class MainWindow(QMainWindow):
 
         view.setModel(model)
         view.resize(441, 571)
+        view.resizeColumnsToContents()
+        view.resizeRowsToContents()
 
         UIFunctions.removeTitleBar(...)
 
@@ -134,6 +143,203 @@ class MainWindow(QMainWindow):
     def locker(self, lock_value):
         pass
 
+class downloader(MainWindow, Ui_MainWindow):
+    def __init__(self, parent=MainWindow):
+        super(downloader).__init__(parent)
+        self.url = None
+
+    def init_args(self, url):
+        self.url = url
+
+
+class Functions(MainWindow, Ui_MainWindow):
+    def __init__(self, parent=MainWindow):
+        super(Functions, self).__init__(parent)
+        self.setupUi(self)
+
+    def start(self):
+        if len(self.ui.lineEdit_2.text()) > 5:
+            if self.download_folder is not None:
+                qlink = self.ui.lineEdit_2.text()
+                dataas = (datetime.datetime.now(), qlink)
+                self.cur.execute('INSERT INTO history VALUES (?,?)', dataas)
+                self.con.commit()
+                self.con.close()
+                self.mythread.init_args(qlink)
+                self.mythread.run()
+                self.locker(False)
+            else:
+                QtWidgets.QMessageBox.warning(self, "Ошибка", "Вы не выбрали папку")
+        else:
+            QtWidgets.QMessageBox.warning(self, "Ошибка", "Ссылка на видео не Указана")
+
+    def get_folder(self):
+        self.download_folder = QtWidgets.QFileDialog.getExistingDirectory(None, 'Выбрать папку для сохранения')
+        os.chdir(self.download_folder)
+
+    def handler(self, value):
+        if value == 'finish':
+            self.locker(False)
+
+        else:
+            self.ui.plainTextEdit_2.appendPlainText(value)
+
+class UIFunctions(MainWindow):
+    GLOBAL_STATE = 0
+    GLOBAL_TITLE_BAR = True
+
+    def maximize_restore(self):
+        global GLOBAL_STATE
+        status = GLOBAL_STATE
+        if status == 0:
+            self.showMaximized()
+            GLOBAL_STATE = 1
+            self.ui.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+            self.ui.btn_maximize_restore.setToolTip("Restore")
+            self.ui.btn_maximize_restore.setIcon(QtGui.QIcon(u":/16x16/icons/16x16/cil-window-restore.png"))
+            self.ui.frame_top_btns.setStyleSheet("background-color: rgb(27, 29, 35)")
+            self.ui.frame_size_grip.hide()
+        else:
+            GLOBAL_STATE = 0
+            self.showNormal()
+            self.resize(self.width() + 1, self.height() + 1)
+            self.ui.horizontalLayout.setContentsMargins(10, 10, 10, 10)
+            self.ui.btn_maximize_restore.setToolTip("Maximize")
+            self.ui.btn_maximize_restore.setIcon(QtGui.QIcon(u":/16x16/icons/16x16/cil-window-maximize.png"))
+            self.ui.frame_top_btns.setStyleSheet("background-color: rgba(27, 29, 35, 200)")
+            self.ui.frame_size_grip.show()
+
+    def returStatus(self):
+        return GLOBAL_STATE
+
+    def setStatus(status):
+        global GLOBAL_STATE
+        GLOBAL_STATE = status
+
+    def enableMaximumSize(self, width, height):
+        if width != '' and height != '':
+            self.setMaximumSize(QSize(width, height))
+            self.ui.frame_size_grip.hide()
+            self.ui.btn_maximize_restore.hide()
+
+    def toggleMenu(self, maxWidth, enable):
+        if enable:
+            width = self.ui.frame_left_menu.width()
+            maxExtend = maxWidth
+            standard = 70
+
+            if width == 70:
+                widthExtended = maxExtend
+            else:
+                widthExtended = standard
+
+            self.animation = QPropertyAnimation(self.ui.frame_left_menu, b"minimumWidth")
+            self.animation.setDuration(300)
+            self.animation.setStartValue(width)
+            self.animation.setEndValue(widthExtended)
+            self.animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
+            self.animation.start()
+
+    def removeTitleBar(status):
+        global GLOBAL_TITLE_BAR
+        GLOBAL_TITLE_BAR = status
+
+    def labelTitle(self, text):
+        self.ui.label_title_bar_top.setText(text)
+
+    def labelDescription(self, text):
+        self.ui.label_top_info_1.setText(text)
+
+    def addNewMenu(self, name, objName, icon, isTopMenu):
+        font = QFont()
+        font.setFamily(u"Segoe UI")
+        button = QPushButton(str(count), self)
+        button.setObjectName(objName)
+        sizePolicy3 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        sizePolicy3.setHorizontalStretch(0)
+        sizePolicy3.setVerticalStretch(0)
+        sizePolicy3.setHeightForWidth(button.sizePolicy().hasHeightForWidth())
+        button.setSizePolicy(sizePolicy3)
+        button.setMinimumSize(QSize(0, 70))
+        button.setLayoutDirection(Qt.LeftToRight)
+        button.setFont(font)
+        button.setStyleSheet(Style.style_bt_standard.replace('ICON_REPLACE', icon))
+        button.setText(name)
+        button.setToolTip(name)
+        button.clicked.connect(self.Button)
+
+        if isTopMenu:
+            self.ui.layout_menus.addWidget(button)
+        else:
+            self.ui.layout_menu_bottom.addWidget(button)
+
+    def selectMenu(getStyle):
+        select = getStyle + ("QPushButton { border-right: 7px solid rgb(44, 49, 60); }")
+        return select
+
+    def deselectMenu(getStyle):
+        deselect = getStyle.replace("QPushButton { border-right: 7px solid rgb(44, 49, 60); }", "")
+        return deselect
+
+    def selectStandardMenu(self, widget):
+        for w in self.ui.frame_left_menu.findChildren(QPushButton):
+            if w.objectName() == widget:
+                w.setStyleSheet(UIFunctions.selectMenu(w.styleSheet()))
+
+    def resetStyle(self, widget):
+        for w in self.ui.frame_left_menu.findChildren(QPushButton):
+            if w.objectName() != widget:
+                w.setStyleSheet(UIFunctions.deselectMenu(w.styleSheet()))
+
+    def labelPage(self, text):
+        newText = '| ' + text.upper()
+        self.ui.label_top_info_2.setText(newText)
+
+    def userIcon(self, initialsTooltip, icon, showHide):
+        if showHide:
+            self.ui.label_user_icon.setText(initialsTooltip)
+
+            if icon:
+                style = self.ui.label_user_icon.styleSheet()
+                setIcon = "QLabel { background-image: " + icon + "; }"
+                self.ui.label_user_icon.setStyleSheet(style + setIcon)
+                self.ui.label_user_icon.setText('')
+                self.ui.label_user_icon.setToolTip(initialsTooltip)
+        else:
+            self.ui.label_user_icon.hide()
+
+    def uiDefinitions(self):
+        def dobleClickMaximizeRestore(event):
+            if event.type() == QtCore.QEvent.MouseButtonDblClick:
+                QtCore.QTimer.singleShot(250, lambda: UIFunctions.maximize_restore(self))
+
+        if GLOBAL_TITLE_BAR:
+            self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+            self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+            self.ui.frame_label_top_btns.mouseDoubleClickEvent = dobleClickMaximizeRestore
+        else:
+            self.ui.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+            self.ui.frame_label_top_btns.setContentsMargins(8, 0, 0, 5)
+            self.ui.frame_label_top_btns.setMinimumHeight(42)
+            self.ui.frame_icon_top_bar.hide()
+            self.ui.frame_btns_right.hide()
+            self.ui.frame_size_grip.hide()
+
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(17)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor(0, 0, 0, 150))
+        self.ui.frame_main.setGraphicsEffect(self.shadow)
+
+        self.sizegrip = QSizeGrip(self.ui.frame_size_grip)
+        self.sizegrip.setStyleSheet("width: 20px; height: 20px; margin 0px; padding: 0px;")
+
+        self.ui.btn_minimize.clicked.connect(lambda: self.showMinimized())
+
+        self.ui.btn_maximize_restore.clicked.connect(lambda: UIFunctions.maximize_restore(self))
+
+        self.ui.btn_close.clicked.connect(lambda: self.close())
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
